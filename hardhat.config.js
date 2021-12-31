99bd36472327ecc0d651170f65cd8bc3680d5d24
Brian Gershon <briangershon@users.noreply.github.com>
2021-12-31 15:28:46 -0800

    Fix tests with new hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,12 +5,16 @@ require("@nomiclabs/hardhat-ethers");
  * @type import('hardhat/config').HardhatUserConfig
  */
 
+// default values are there to avoid failures when running tests
+const POLYGON_MUMBAI_RPC = process.env.POLYGON_MUMBAI_RPC || "1".repeat(32);
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "1".repeat(64);
+
 module.exports = {
   solidity: "0.8.9",
   networks: {
     mumbai: {
-      url: process.env.POLYGON_MUMBAI_RPC,
-      accounts: [process.env.PRIVATE_KEY],
+      url: POLYGON_MUMBAI_RPC,
+      accounts: [PRIVATE_KEY],
     },
   },
 };
