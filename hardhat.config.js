ddabefaa505e446ce65042921002d2aec8d79c93
Brian Gershon <briangershon@users.noreply.github.com>
2022-05-17 20:43:59 -0700

    Run Prettier on codebase

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,28 +1,28 @@
-require("dotenv").config();
-require("@nomiclabs/hardhat-ethers");
+require('dotenv').config();
+require('@nomiclabs/hardhat-ethers');
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 
 // default values are there to avoid failures when running tests
-const POLYGON_MUMBAI_RPC = process.env.POLYGON_MUMBAI_RPC || "1".repeat(32);
-const PRIVATE_KEY = process.env.PRIVATE_KEY || "1".repeat(64);
+const POLYGON_MUMBAI_RPC = process.env.POLYGON_MUMBAI_RPC || '1'.repeat(32);
+const PRIVATE_KEY = process.env.PRIVATE_KEY || '1'.repeat(64);
 
 module.exports = {
-  solidity: {
-    version: "0.8.9",
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 1000,
-      },
+    solidity: {
+        version: '0.8.9',
+        settings: {
+            optimizer: {
+                enabled: true,
+                runs: 1000,
+            },
+        },
     },
-  },
-  networks: {
-    mumbai: {
-      url: POLYGON_MUMBAI_RPC,
-      accounts: [PRIVATE_KEY],
+    networks: {
+        mumbai: {
+            url: POLYGON_MUMBAI_RPC,
+            accounts: [PRIVATE_KEY],
+        },
     },
-  },
 };
